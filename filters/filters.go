136dbb41e9344d406127a78bbeef5974d14af88b
Zoltan Nagy <abesto@abesto.net>
2015-06-16 14:59:50 +0200

    Fix (first) filter

--- filters/filters.go
+++ filters/filters.go
@@ -132,7 +132,7 @@ type first struct{}
 
 func (f *first) Filter(targets []target.Target) []target.Target {
 	if len(targets) > 0 {
-		return targets[1:]
+		return targets[0:1]
 	}
 	return targets
 }
